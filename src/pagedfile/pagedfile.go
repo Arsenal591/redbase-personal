ca586354c9c443d341c30824683ff392f57252d1
wangjinglong <arsenal591@126.com>
2020-09-18 00:19:05 +0800

    paged file: basic file handler declaration.

---
+++ src/pagedfile/pagedfile.go
@@ -0,0 +1,54 @@
+package pagedfile
+
+import "os"
+
+const (
+	NonExistPageNum = -1
+)
+
+// FileHeader always lies on the first page of a file, providing necessary page information.
+type FileHeader struct {
+	FirstFreePage int32 // Page number of a file's first free page.
+	NumPages      int32 // Number of pages (including header page)
+}
+
+func NewFileHeader() *FileHeader {
+	return &FileHeader{
+		FirstFreePage: NonExistPageNum,
+		NumPages:      1,
+	}
+}
+
+type FileHandler struct {
+	hdr     *FileHeader
+	bufPool *BufferPool
+	fi      *os.File
+}
+
+func NewFileHandler(fi *os.File, pool *BufferPool) (*FileHandler, error) {
+	page, err := pool.getPage(fi, FileHeaderPageNum, false)
+	if err != nil {
+		return nil, err
+	}
+	hdrMgr, err := NewFileHeaderMgr(page.memBuffer)
+	if err != nil {
+		return nil, err
+	}
+	return &FileHandler{
+		hdrMgr:  hdrMgr,
+		bufPool: pool,
+		fi:      fi,
+	}, nil
+
+}
+
+func (fh *FileHandler) Close() error {
+	err := fh.bufPool.CloseFile(fh)
+	if err != nil {
+		return err
+	}
+	fh.hdrMgr = nil
+	fh.bufPool = nil
+	fh.fi = nil
+	return nil
+}
